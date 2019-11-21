31ccbe9392a5b12f094009a05be13ac850a9db35
JangJur <wkdwndud753@naver.com>
2019-11-21 17:02:07 +0900

    bootstrap update(page x)

--- src/components/MovieInput.vue
+++ src/components/MovieInput.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="inputBox shadow">
-    <input
-      type="text"
-      v-model="searchTitle"
-      placeholder="What do you want to Movie Title"
-      v-on:keyup.enter="searchMovie"
-    />
-    <span class="searchContainer" v-on:click="searchMovie">
-      <i class="searchBtn fas fa-search" aria-hidden="true"></i>
-    </span>
-    <span class="refreshContainer" v-on:click="clearInput">
-      <i class="refreshBtn fas fa-sync-alt" aria-hidden="true"></i>
-    </span>
+  <div>
+    <b-row>
+      <b-col sm="3">
+      </b-col>
+      <b-col sm="6">
+        <b-form-input class="text-center" v-model="searchTitle" v-on:keyup.enter="searchMovie" placeholder="What do you want to Movie Title"></b-form-input>
+      </b-col>
+      <b-col sm="3">
+        <b-button variant="outline-primary" v-on:click="clearInput"><i class="searchBtn fas fa-sync-alt"></i></b-button>
+        <b-button variant="outline-primary" v-on:click="searchMovie"><i class="searchBtn fas fa-search"></i></b-button>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -36,47 +35,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-input:focus {
-  outline: none;
-}
-.inputBox {
-  float: center;
-  text-align: center;
-  margin-left: auto;
-  margin-right: auto;
-  background: white;
-  height: 50px;
-  line-height: 50px;
-  border-radius: 5px;
-  width: 50%;
-}
-.inputBox input {
-  text-align: center;
-  border-style: none;
-  font-size: 0.9rem;
-  width: 60%;
-}
-.refreshContainer {
-  float: right;
-  background: linear-gradient(to right, #6478fb, #8763fb);
-  display: block;
-  width: 40px;
-}
-.refreshBtn {
-  color: white;
-  vertical-align: middle;
-}
-.searchContainer {
-  float: right;
-  background: linear-gradient(to right, #6478fb, #8763fb);
-  display: block;
-  width: 40px;
-  border-radius: 0 5px 5px 0;
-}
-.searchBtn {
-  color: white;
-  vertical-align: middle;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { store } from './store'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Paginate from 'vuejs-paginate'
+
+Vue.use(BootstrapVue)
+Vue.component('paginate', Paginate)
 
 new Vue({
   el: '#app',
